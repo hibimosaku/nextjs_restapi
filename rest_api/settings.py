51140df76c4a513a36aa334a042d9a62c1e6344d
hibimosaku <n.kubomitsu@gmail.com>
2022-12-18 09:49:07 +0900

    render6

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -69,6 +69,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
